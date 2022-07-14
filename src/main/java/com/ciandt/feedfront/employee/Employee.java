1b0a1cc25169f256a8ff7d4b074c624f4fb5d948
Carolina Bartoli <cbartoli@ciandt.com>
2022-07-14 17:01:36 -0300

    Pendente metodos Salvar, Atualizar, Listar, Buscar e Apagar

--- src/main/java/com/ciandt/feedfront/employee/Employee.java
+++ src/main/java/com/ciandt/feedfront/employee/Employee.java
@@ -5,20 +5,30 @@ import com.ciandt.feedfront.excecoes.ComprimentoInvalidoException;
 import com.ciandt.feedfront.excecoes.EmailInvalidoException;
 import com.ciandt.feedfront.excecoes.EmployeeNaoEncontradoException;
 
+import java.io.*;
 import java.util.List;
 import java.util.UUID;
 
-public class Employee {
+public class Employee implements Serializable {
+    private static final long serialVersionUID = -525601868125339430L;
     private String id;
     private String nome;
     private String sobrenome;
     private String email;
+    static String arquivo;
 
-    String arquivoCriado = "arquivo.extensao"; //TODO: alterar de acordo com a sua implementação
+    static String arquivoCriado = "arquivo.extensao"; //TODO: alterar de acordo com a sua implementação
+    static final String repositorioPath = "/home/cbartoli/Documentos/Apps/git/feedfront/src/main/resources/data/";
 
     public Employee(String nome, String sobrenome, String email) throws ComprimentoInvalidoException {
         this.id = UUID.randomUUID().toString();
+        setNome(nome);
+        setSobrenome(sobrenome);
+        setEmail(email);
+        arquivo = repositorioPath+id+".byte";
     }
+
+    public Employee(){}
 
     public static Employee salvarEmployee(Employee employee) throws ArquivoException, EmailInvalidoException {
         return null;
@@ -40,24 +50,35 @@ public class Employee {
     }
 
     public String getNome() {
-        return null;
+        return nome;
     }
 
     public void setNome(String nome) throws ComprimentoInvalidoException {
+        if(nome.length() < 2){
+            throw new ComprimentoInvalidoException("Comprimento do nome deve ser maior que 2 caracteres.");
+        } else {
+            this.nome = nome;
+        }
     }
 
     public String getSobrenome() {
-        return null;
+        return sobrenome;
     }
 
     public void setSobrenome(String sobrenome) throws ComprimentoInvalidoException {
+        if(sobrenome.length() < 2){
+            throw new ComprimentoInvalidoException("Comprimento do sobrenome deve ser maior que 2 caracteres.");
+        } else {
+            this.sobrenome = sobrenome;
+        }
     }
 
     public String getEmail() {
-        return null;
+        return email;
     }
 
     public void setEmail(String email) {
+        this.email = email;
     }
 
     public String getId() {
