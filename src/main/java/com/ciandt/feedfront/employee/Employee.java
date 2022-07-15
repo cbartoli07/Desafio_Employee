a9a995c82a49ca86fadf12fea8cf13fa0e332c96
Carolina Bartoli <cbartoli@ciandt.com>
2022-07-15 14:40:42 -0300

    Pendentes Atualizar, Buscar e Listar, files.byte sendo criados mas não lidos

--- src/main/java/com/ciandt/feedfront/employee/Employee.java
+++ src/main/java/com/ciandt/feedfront/employee/Employee.java
@@ -6,7 +6,9 @@ import com.ciandt.feedfront.excecoes.EmailInvalidoException;
 import com.ciandt.feedfront.excecoes.EmployeeNaoEncontradoException;
 
 import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
+import java.util.ListIterator;
 import java.util.UUID;
 
 public class Employee implements Serializable {
@@ -16,9 +18,8 @@ public class Employee implements Serializable {
     private String sobrenome;
     private String email;
     static String arquivo;
-
-    static String arquivoCriado = "arquivo.extensao"; //TODO: alterar de acordo com a sua implementação
-    static final String repositorioPath = "/home/cbartoli/Documentos/Apps/git/feedfront/src/main/resources/data/";
+    static String repositorioPath = "/home/cbartoli/Documentos/Apps/git/feedfront/src/main/resources/data/";
+    private static String arquivoCriado = "arquivo.extensao"; //TODO: alterar de acordo com a sua implementação
 
     public Employee(String nome, String sobrenome, String email) throws ComprimentoInvalidoException {
         this.id = UUID.randomUUID().toString();
@@ -28,9 +29,43 @@ public class Employee implements Serializable {
         arquivo = repositorioPath+id+".byte";
     }
 
-    public Employee(){}
-
     public static Employee salvarEmployee(Employee employee) throws ArquivoException, EmailInvalidoException {
+        //arrumar a excessão de email
+//        arquivo = repositorioPath+employee.getId()+".byte";
+//        try{
+//            FileOutputStream fileOut = new FileOutputStream(arquivo);
+//            ObjectOutputStream out = new ObjectOutputStream(fileOut);
+//            out.writeObject(employee);
+//            out.close();
+//            fileOut.close();
+//            return employee;
+//        } catch (IOException i) {
+//            throw new ArquivoException("Arquivo não encontrado");
+//        }
+        return null;
+    }
+
+    public static Employee buscarEmployee(String id) throws ArquivoException, EmployeeNaoEncontradoException {
+//        arquivo = repositorioPath+id+".byte";
+//        try {
+//            FileInputStream fileOutput = new FileInputStream(arquivo);
+//            ObjectInputStream ois = new ObjectInputStream(fileOutput);
+//            Employee employee = (Employee) ois.readObject();
+//            return employee;
+//        } catch (FileNotFoundException e) {
+//            throw new ArquivoException("Arquivo não encontrado");
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        } catch (ClassNotFoundException e) {
+//            throw new EmployeeNaoEncontradoException("Employee não encontrado");
+//        }
+        return null;
+    }
+
+    public static void apagarEmployee(String id) throws ArquivoException, EmployeeNaoEncontradoException {
+    }
+
+    public static List<Employee> listarEmployees() throws ArquivoException {
         return null;
     }
 
@@ -38,23 +73,12 @@ public class Employee implements Serializable {
         return null;
     }
 
-    public static List<Employee> listarEmployees() throws ArquivoException {
-        return null;
-    }
-
-    public static Employee buscarEmployee(String id) throws ArquivoException, EmployeeNaoEncontradoException {
-        return null;
-    }
-
-    public static void apagarEmployee(String id) throws ArquivoException, EmployeeNaoEncontradoException {
-    }
-
     public String getNome() {
         return nome;
     }
 
     public void setNome(String nome) throws ComprimentoInvalidoException {
-        if(nome.length() < 2){
+        if(nome.length() <= 2){
             throw new ComprimentoInvalidoException("Comprimento do nome deve ser maior que 2 caracteres.");
         } else {
             this.nome = nome;
@@ -66,7 +90,7 @@ public class Employee implements Serializable {
     }
 
     public void setSobrenome(String sobrenome) throws ComprimentoInvalidoException {
-        if(sobrenome.length() < 2){
+        if(sobrenome.length() <= 2){
             throw new ComprimentoInvalidoException("Comprimento do sobrenome deve ser maior que 2 caracteres.");
         } else {
             this.sobrenome = sobrenome;
@@ -85,4 +109,13 @@ public class Employee implements Serializable {
         return id;
     }
 
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id='" + id + '\'' +
+                ", nome='" + nome + '\'' +
+                ", sobrenome='" + sobrenome + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
 }
